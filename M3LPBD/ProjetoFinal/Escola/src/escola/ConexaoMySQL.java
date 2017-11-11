bb20440fa2badbd09eb4788f49f9f6e432c63e10
celsodavid <celunico43@gmail.com>
2017-11-11 16:45:38 -0200

    projeto final

---
+++ M3LPBD/ProjetoFinal/Escola/src/escola/ConexaoMySQL.java
@@ -0,0 +1,83 @@
+//******************************************************
+//Instituto Federal de São Paulo - Campus Sertãozinho
+//Disciplina......: M3LPBD
+//Programação de Computadores e Dispositivos Móveis
+//Aluno...........: Celso David Lopes dos Santos
+//******************************************************
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escola;
+
+/**
+ * Classes necessarias para uso de Banco de dados
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author celso
+ */
+public class ConexaoMySQL {
+    private static ConexaoMySQL instance = null;
+    private Connection connection = null;
+    
+    private ConexaoMySQL() {
+        try {            
+            String driverName = "com.mysql.jdbc.Driver";
+            Class.forName(driverName);
+            
+            String serverName = "127.0.0.1";    
+            
+            //nesta linha estará o nome do seu banco de dados. Olhe no workbench
+            //o nome do banco. Caso voce tenha criado o modelo no workbench e não
+            //tenha modificado o nome do banco, o nome padrão é "mydb". 
+            String dbName = "Escola" ;        
+            String url = "jdbc:mysql://" + 
+                    serverName + "/" + 
+                    dbName;
+            
+            //ajuste o username e password de acordo com o que voce utiliza para 
+            //acessar o banco
+            String username = "root";             
+            String password = "admin";      
+            
+            connection = DriverManager.getConnection(url,
+                    username, password);
+              
+            if (connection != null) {
+                System.out.println("STATUS--->Conectado "
+                        + "com sucesso!");
+            } else {
+                System.err.println("STATUS--->Nao foi "
+                        + "possivel realizar conexao");
+            }
+            connection.setAutoCommit(true);
+        } catch (ClassNotFoundException e) {  
+            
+            System.out.println("O driver expecificado"
+                    + " nao foi encontrado.");
+        } catch (SQLException e) {
+            
+            System.out.println("Nao foi possivel"
+                    + " conectar ao Banco de Dados.");
+            e.printStackTrace();
+        }
+        }
+
+    public static ConexaoMySQL getInstance() {
+        if (instance == null) {
+            instance = new ConexaoMySQL();
+        }
+        return instance;
+    }
+
+    public java.sql.Connection getConnection() {
+        return connection;
+    }
+}
---
+++ M3LPBD/ProjetoFinal/Escola/src/escola/Escola.java
@@ -0,0 +1,28 @@
+//******************************************************
+//Instituto Federal de São Paulo - Campus Sertãozinho
+//Disciplina......: M3LPBD
+//Programação de Computadores e Dispositivos Móveis
+//Aluno...........: Celso David Lopes dos Santos
+//******************************************************
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escola;
+
+/**
+ *
+ * @author celso
+ */
+public class Escola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
